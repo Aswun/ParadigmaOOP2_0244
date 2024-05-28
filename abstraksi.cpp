4b27ba3bd6e37c4ba103fe7efe814258b318e4b6
Aswun <aswinlutfian32@gmail.com>
2024-05-28 15:58:28 +0700

    menambahkan class abstraksi

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -1,2 +1,23 @@
 #include <iostream>
 using namespace std;
+
+class abstraksiKlas
+{
+private:
+    string x, y;
+public:
+
+    //method untuk mengisi nilai
+    //private member
+    void setXY(string a, string b)
+    {
+        x = a;
+        y = b;
+    }
+    //menampilkan nilai
+    void display()
+    {
+        cout << "x = " << x << endl;
+        cout << "y = " << y << endl; 
+    }
+};
